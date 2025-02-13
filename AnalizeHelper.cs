e36dc2759ee7e0de2103a1415246664b85e06229
冯 <2789083329@qq.com>
2025-02-13 11:56:17 +0800

    添加项目文件。

---
+++ AnalizeHelper.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FastHotKeyForWPF.Generator
+{
+    internal static class AnalizeHelper
+    {
+        internal static bool IsPartialClass(SyntaxNode node)
+        {
+            return node is ClassDeclarationSyntax classDecl && classDecl.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword));
+        }
+        internal static bool IsAopClass(ClassDeclarationSyntax classDecl)
+        {
+            return classDecl.Members
+                .OfType<MemberDeclarationSyntax>()
+                .Any(member => member.AttributeLists
+                    .SelectMany(al => al.Attributes)
+                    .Any(attr => attr.Name.ToString() == "AspectOriented"));
+        }
+        internal static bool FindAttribute(ClassDeclarationSyntax classDecl, string attributeName)
+        {
+            return classDecl.Members
+                .OfType<MemberDeclarationSyntax>()
+                .Any(member => member.AttributeLists
+                    .SelectMany(al => al.Attributes)
+                    .Any(attr => attr.Name.ToString() == attributeName));
+        }
+        internal static bool FindAttribute(IFieldSymbol fieldSymbol, string attributeName)
+        {
+            foreach (var attribute in fieldSymbol.GetAttributes())
+            {
+                if (attribute.AttributeClass == null) continue;
+
+                if (attribute.AttributeClass.AllInterfaces.Any(i => i.Name == attributeName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal static ClassDeclarationSyntax GetClassDeclaration(GeneratorSyntaxContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)context.Node;
+            return classDeclaration;
+        }
+        internal static IncrementalValuesProvider<ClassDeclarationSyntax> DefiningFilter(IncrementalGeneratorInitializationContext context)
+        {
+            IncrementalValuesProvider<ClassDeclarationSyntax> classDeclarations =
+                context.SyntaxProvider.CreateSyntaxProvider(
+                    predicate: static (node, cancellationToken) => IsPartialClass(node),
+                    transform: static (context, cancellationToken) => GetClassDeclaration(context))
+                .Where(static m => m != null)!;
+            return classDeclarations;
+        }
+        internal static IncrementalValueProvider<(Compilation Compilation, ImmutableArray<ClassDeclarationSyntax> Classes)> GetValue(IncrementalGeneratorInitializationContext context, IncrementalValuesProvider<ClassDeclarationSyntax> classDeclarations)
+        {
+            IncrementalValueProvider<(Compilation Compilation, ImmutableArray<ClassDeclarationSyntax> Classes)> compilationAndClasses =
+                context.CompilationProvider.Combine(classDeclarations.Collect());
+            return compilationAndClasses;
+        }
+        internal static string GetNamespace(ClassDeclarationSyntax classDeclaration)
+        {
+            SyntaxNode? current = classDeclaration;
+            while (current != null && current is not NamespaceDeclarationSyntax)
+            {
+                current = current.Parent;
+            }
+
+            return current is NamespaceDeclarationSyntax namespaceDeclaration
+                ? namespaceDeclaration.Name.ToString()
+                : "Global";
+        }
+        internal static string GetInterfaceName(ClassDeclarationSyntax classDeclarationSyntax)
+        {
+            return $"IAop{classDeclarationSyntax.Identifier.Text}In{GetNamespace(classDeclarationSyntax).Replace('.', '_')}";
+        }
+        internal static string GetPropertyNameByFieldName(VariableDeclaratorSyntax variable)
+        {
+            if (variable.Identifier.Text.StartsWith("_"))
+            {
+                return char.ToUpper(variable.Identifier.Text[1]) + variable.Identifier.Text.Substring(2);
+            }
+            else
+            {
+                return char.ToUpper(variable.Identifier.Text[0]) + variable.Identifier.Text.Substring(1);
+            }
+        }
+        internal static string InitialTextParse(this string source)
+        {
+            return source.Replace('=', ' ').TrimStart();
+        }
+        internal static string GetAccessModifier(INamedTypeSymbol symbol)
+        {
+            switch (symbol.DeclaredAccessibility)
+            {
+                case Accessibility.Public:
+                    return "public";
+                case Accessibility.Internal:
+                    return "internal";
+                case Accessibility.Private:
+                    return "private";
+                case Accessibility.Protected:
+                    return "protected";
+                case Accessibility.ProtectedAndInternal:
+                    return "protected internal";
+                case Accessibility.ProtectedOrInternal:
+                    return "private protected";
+                default:
+                    return "internal";
+            }
+        }
+    }
+}
---
+++ ClassRoslyn.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MinimalisticWPF.Generator
+{
+    internal class ClassRoslyn
+    {
+        internal ClassRoslyn(ClassDeclarationSyntax classDeclarationSyntax, INamedTypeSymbol namedTypeSymbol)
+        {
+            Syntax = classDeclarationSyntax;
+            Symbol = namedTypeSymbol;
+        }
+
+        public ClassDeclarationSyntax Syntax { get; private set; }
+        public INamedTypeSymbol Symbol { get; private set; }
+
+        public static HashSet<string> GetReferencedNamespaces(INamedTypeSymbol namedTypeSymbol)
+        {
+            HashSet<string> namespaces = [];
+
+            var syntaxRef = namedTypeSymbol.DeclaringSyntaxReferences.FirstOrDefault();
+            if (syntaxRef == null)
+            {
+                return namespaces;
+            }
+            var classDecl = syntaxRef.GetSyntax() as ClassDeclarationSyntax;
+            if (classDecl == null)
+            {
+                return namespaces;
+            }
+            var compilationUnit = classDecl.FirstAncestorOrSelf<CompilationUnitSyntax>();
+            if (compilationUnit == null)
+            {
+                return namespaces;
+            }
+
+            foreach (var usingDirective in compilationUnit.Usings)
+            {
+                if (usingDirective != null)
+                {
+                    if (usingDirective.Name != null)
+                    {
+                        namespaces.Add($"using {usingDirective.Name};");
+                    }
+                }
+            }
+
+            return namespaces;
+        }
+
+        public string GenerateUsing()
+        {
+            StringBuilder sourceBuilder = new();
+            sourceBuilder.AppendLine("#nullable enable");
+            sourceBuilder.AppendLine();
+            var hashUsings = GetReferencedNamespaces(Symbol);
+            hashUsings.Add("using FastHotKeyForWPF.Custom;");
+            foreach (var use in hashUsings)
+            {
+                sourceBuilder.AppendLine(use);
+            }
+            sourceBuilder.AppendLine();
+            return sourceBuilder.ToString();
+        }
+        public string GenerateNamespace()
+        {
+            StringBuilder sourceBuilder = new();
+            sourceBuilder.AppendLine($"namespace {Symbol.ContainingNamespace}");
+            sourceBuilder.AppendLine("{");
+            return sourceBuilder.ToString();
+        }
+        public string GeneratePartialClass(bool ignoreViewModel = false, bool ignorePool = false, bool ignoreTheme = false)
+        {
+            StringBuilder sourceBuilder = new();
+            string share = $"   {Syntax.Modifiers} class {Syntax.Identifier.Text} : IHotKeyComponent";
+            sourceBuilder.AppendLine(share);
+            sourceBuilder.AppendLine("   {");
+
+            return sourceBuilder.ToString();
+        }
+        public string GenerateEnd()
+        {
+            StringBuilder sourceBuilder = new();
+            sourceBuilder.AppendLine("   }");
+            sourceBuilder.AppendLine("}");
+            return sourceBuilder.ToString();
+        }
+    }
+}
